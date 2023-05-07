56f9951613d1596f8ccbccf6223d9d568d023b14
NatKos <112882937+NatKos02@users.noreply.github.com>
2023-05-07 12:05:38 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const nav = document.querySelector('.navbar');
-	const allNavItems = document.querySelectorAll('.nav-link');
-	const navList = document.querySelector('.navbar-collapse');
-
-	allNavItems.forEach((item) =>
-		item.addEventListener('click', () => navList.classList.remove('show'))
-	);
-});
